0b763d2df1c32b4c959be3fdc085f2c00d330087
easywriter21 <sajalwaghmare54@gmail.com>
2026-03-21 07:56:18 +0530

    Update ai_engine.py

--- utils/ai_engine.py
+++ utils/ai_engine.py
@@ -16,7 +16,7 @@ def ask_estimator(user_input):
     }
 
     data = {
-        "model": "mixtral-8x7b-32768",
+        "model": "llama3-70b-8192",
         "messages": [
             {
                 "role": "system",
