385b2f88cd3742aa440cbcd874ae3c507e54594f
easywriter21 <sajalwaghmare54@gmail.com>
2026-03-26 22:30:25 +0530

    Update boq_generator.py

--- utils/boq_generator.py
+++ utils/boq_generator.py
@@ -1,33 +1,8 @@
 def generate_boq(area):
 
     return [
-
-        {
-            "item": "Earthwork Excavation",
-            "unit": "m3",
-            "quantity": area * 0.03,
-            "rate": 250
-        },
-
-        {
-            "item": "PCC",
-            "unit": "m3",
-            "quantity": area * 0.01,
-            "rate": 5500
-        },
-
-        {
-            "item": "RCC Concrete",
-            "unit": "m3",
-            "quantity": area * 0.02,
-            "rate": 7500
-        },
-
-        {
-            "item": "Brickwork",
-            "unit": "m3",
-            "quantity": area * 0.05,
-            "rate": 6500
-        }
-
+        {"item": "Excavation", "unit": "m3", "quantity": area*0.03, "rate": 250, "cost": area*0.03*250},
+        {"item": "PCC", "unit": "m3", "quantity": area*0.01, "rate": 5500, "cost": area*0.01*5500},
+        {"item": "RCC", "unit": "m3", "quantity": area*0.02, "rate": 7500, "cost": area*0.02*7500},
+        {"item": "Brickwork", "unit": "m3", "quantity": area*0.05, "rate": 6500, "cost": area*0.05*6500}
     ]
