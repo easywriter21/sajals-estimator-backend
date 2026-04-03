b9f29acb099c015d30fb887e6c13935a54980911
easywriter21 <sajalwaghmare54@gmail.com>
2026-04-03 18:02:40 +0530

    Update excel.py

--- routes/excel.py
+++ routes/excel.py
@@ -1,29 +1,11 @@
 from fastapi import APIRouter
 from fastapi.responses import FileResponse
-import pandas as pd
-from utils.cost_calculator import calculate_cost
-from openpyxl import load_workbook
-from openpyxl.styles import Font
+from services.cost_calculator import generate_excel
 
-router = APIRouter(prefix="/excel")
+router = APIRouter()
 
-@router.post("/")
-def export_excel(data: dict):
 
-    result = calculate_cost(data)
-    df = pd.DataFrame(result["boq"])
-
-    file_name = "BOQ.xlsx"
-
-    df.to_excel(file_name, index=False)
-
-    wb = load_workbook(file_name)
-    ws = wb.active
-
-    # Bold header
-    for cell in ws[1]:
-        cell.font = Font(bold=True)
-
-    wb.save(file_name)
-
-    return FileResponse(file_name, filename=file_name)
+@router.post("/excel/")
+def excel():
+    file_path = generate_excel()
+    return FileResponse(file_path, filename="Estimate.xlsx")
