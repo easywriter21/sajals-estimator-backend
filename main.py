7b501918cb300510ed46c50df3923389f40ce34f
easywriter21 <sajalwaghmare54@gmail.com>
2026-04-03 18:22:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,13 @@ from routes import estimate, excel, cad, pdf
 
 app = FastAPI()
 
-# Static + Templates
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-# Routes
 app.include_router(estimate.router)
 app.include_router(excel.router)
 app.include_router(cad.router)
 app.include_router(pdf.router)
-
 
 @app.get("/")
 def home(request: Request):
