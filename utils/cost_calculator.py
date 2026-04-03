45c67897427a835e4925762da255a788a36c946f
easywriter21 <sajalwaghmare54@gmail.com>
2026-04-03 18:11:20 +0530

    Update cost_calculator.py

--- utils/cost_calculator.py
+++ utils/cost_calculator.py
@@ -1,41 +1,17 @@
-from utils.boq_generator import generate_boq
+from openpyxl import Workbook
 
-def calculate_cost(data):
 
-    area = data.get("builtUpArea", 1200)
-    floors = data.get("floors", 2)
+def generate_excel():
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Estimate"
 
-    budget = data.get("budget")
-    duration = data.get("duration")
+    ws.append(["Item", "Cost"])
+    ws.append(["Material", 500000])
+    ws.append(["Labour", 200000])
+    ws.append(["Total", 700000])
 
-    total_area = area * floors
+    file_path = "estimate.xlsx"
+    wb.save(file_path)
 
-    base_cost = 1800
-
-    # 🔥 Constraint logic
-    if budget:
-        cost_per_sqft = budget / total_area
-    else:
-        cost_per_sqft = base_cost
-
-    total_cost = total_area * cost_per_sqft
-
-    cement = total_area * 0.4
-    steel = total_area * 4
-
-    if duration:
-        labour = "High Speed Work"
-    else:
-        duration = total_area / 50
-        labour = "Normal Work"
-
-    return {
-        "total_area": total_area,
-        "cost_per_sqft": round(cost_per_sqft),
-        "estimated_cost": round(total_cost),
-        "cement_bags": round(cement),
-        "steel_kg": round(steel),
-        "duration_days": round(duration),
-        "labour_type": labour,
-        "boq": generate_boq(total_area)
-    }
+    return file_path
