3f08c793d2ce4a685e540666d48998823d4baeaa
easywriter21 <sajalwaghmare54@gmail.com>
2026-04-07 18:17:42 +0530

    Update master_estimator.py

--- utils/master_estimator.py
+++ utils/master_estimator.py
@@ -1,4 +1,20 @@
-from utils.ai_engine import generate_estimate
-
 def master_estimate(text: str, prices: dict):
-    return generate_estimate(text, prices)
+    # Simple stable logic
+
+    cement_cost = 400 * prices["cement"]
+    steel_cost = 2 * prices["steel"]
+    sand_cost = 100 * prices["sand"]
+    aggregate_cost = 120 * prices["aggregate"]
+    labour_cost = 50 * prices["labour"]
+
+    total_cost = cement_cost + steel_cost + sand_cost + aggregate_cost + labour_cost
+
+    return {
+        "project": text,
+        "cement_cost": cement_cost,
+        "steel_cost": steel_cost,
+        "sand_cost": sand_cost,
+        "aggregate_cost": aggregate_cost,
+        "labour_cost": labour_cost,
+        "total_cost": total_cost
+    }
