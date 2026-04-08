be8baec189a7298b31fe7d32d5308a58a065b54e
easywriter21 <sajalwaghmare54@gmail.com>
2026-04-08 18:54:19 +0530

    Update pdf_parser.py

--- utils/pdf_parser.py
+++ utils/pdf_parser.py
@@ -1,1 +1,26 @@
+import fitz
 
+def extract_data_from_pdf(file):
+    doc = fitz.open(stream=file, filetype="pdf")
+    text = ""
+
+    for page in doc:
+        text += page.get_text()
+
+    text = text.lower()
+
+    area = None
+    floors = None
+
+    for line in text.split("\n"):
+        if "area" in line:
+            nums = [int(s) for s in line.split() if s.isdigit()]
+            if nums:
+                area = nums[0]
+
+        if "floor" in line:
+            nums = [int(s) for s in line.split() if s.isdigit()]
+            if nums:
+                floors = nums[0]
+
+    return area, floors
