b31ef605b4490f8b812a2952723ef32f13c51c63
easywriter21 <sajalwaghmare54@gmail.com>
2026-04-08 18:45:46 +0530

    Update cost_engine.py

--- utils/cost_engine.py
+++ utils/cost_engine.py
@@ -1,1 +1,7 @@
-
+def calculate_cost(qty, rates):
+    return {
+        "cement": qty["cement"] * rates["cement"],
+        "steel": qty["steel"] * rates["steel"],
+        "sand": qty["sand"] * rates["sand"],
+        "aggregate": qty["aggregate"] * rates["aggregate"]
+    }
