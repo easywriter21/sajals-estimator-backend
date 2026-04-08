6ce50da71b7862939f29c73b08a7b7f002588626
easywriter21 <sajalwaghmare54@gmail.com>
2026-04-08 18:23:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,17 @@ from fastapi.templating import Jinja2Templates
 import os
 
 from routes.estimate import router as estimate_router
+from routes.excel import router as excel_router
 
 app = FastAPI()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Static + Templates
 app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
 templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
-# Routes
 app.include_router(estimate_router)
-
+app.include_router(excel_router)
 
 @app.get("/")
 async def home(request: Request):
