f2bac87396d30e224d4bb86817dea2f9a71b4e6e
easywriter21 <sajalwaghmare54@gmail.com>
2026-04-08 18:40:14 +0530

    Update quantity_engine.py

--- utils/quantity_engine.py
+++ utils/quantity_engine.py
@@ -1,1 +1,9 @@
+def calculate_quantities(area, floors):
+    total = area * floors
 
+    return {
+        "cement": total * 0.4,
+        "steel": total * 3.5,
+        "sand": total * 0.015,
+        "aggregate": total * 0.025
+    }
